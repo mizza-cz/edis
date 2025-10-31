5cc028dbe5c26e1bb07af535e3e56766dd7ddae8
mizza-cz <mizza.cz666@gmail.com>
2025-10-31 06:34:07 +0100

    fix slider categories/static

--- src/components/sliderCategories/sliderCategories.js
+++ src/components/sliderCategories/sliderCategories.js
@@ -2,17 +2,13 @@ const sliderCategories = document.querySelector(".sliderCategories");
 if (sliderCategories) {
   new Swiper(".sliderCategories", {
     slidesPerView: "auto",
-    centeredSlides: true,
 
-    loop: true,
+    loop: false,
     speed: 700,
     navigation: {
       nextEl: ".sliderCategories__button.next",
       prevEl: ".sliderCategories__button.prev",
     },
-    breakpoints: {
-      320: { centeredSlides: false },
-      1440: { centeredSlides: true },
-    },
+    breakpoints: {},
   });
 }
